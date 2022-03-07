67b6e1a343285f4a982d41d86b74e8632ad01d2f
Lucas Salvador Amorim <lucassalvadoramorim@gmail.com>
2022-03-07 17:26:38 +0000

    trabalhando com dados

---
+++ Media.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Media {
+    public static void main(String[] args){
+        double numero1;
+        double numero2;
+        double media;
+
+        Scanner leitura = new Scanner(System.in);
+
+        System.out.println("Digite um numero: " );
+        numero1 = leitura.nextDouble();
+
+        System.out.println("Digite outro numero: " );
+        numero2 = leitura.nextDouble();
+
+        media = (numero1 + numero2) /2.0;
+        System.out.printf("Média =%.2f", media );
+    }
+
+}
